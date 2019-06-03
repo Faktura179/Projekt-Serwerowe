59cdd210782b30bf94e303f20e2a1969a99902bb
Faktura179 <rfatula12@gmail.com>
2019-06-03 16:55:32 +0200

    Nie dziala nic, nie wiem dlaczego :(

--- server.js
+++ server.js
@@ -4,11 +4,25 @@ var http = require('http').createServer(app);
 var socketio = require('socket.io')(http);
 
 app.use(express.static("static"))
+var players = []
 
 socketio.on('connection', function (client) {
     console.log("klient się podłączył " + client.id)
     // client.id - unikalna nazwa klienta generowana przez socket.io
+    if(players.length<2){
+        players.push(client.id)
+    }
+    
 
+    console.log(players)
+    client.emit("connect",{a:true, b: false})
+
+    client.on("move", function (data) {
+        client.broadcast.emit("move", data)
+    })
+    client.on("disconnect", function () {
+        console.log("klient się rozłącza")
+    })
 });
 
 http.listen(3000, function () {
--- static/js/Net.js
+++ static/js/Net.js
@@ -1,5 +1,15 @@
 class Net{
     constructor(){
-        
+        this.io = io()
+        this.myMove=false
+        this.io.on("connect", function(data){
+            console.log(data)
+            //this.myMove=data.move
+            console.log(this.myMove)
+        }.bind(this))
+    }
+
+    move(argumenty){
+        this.io.emit("move",argumenty)
     }
 }
