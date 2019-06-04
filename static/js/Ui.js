620d63628d2509060e706b02c3a330a8a34cb1d3
mikord45 <51155742+mikord45@users.noreply.github.com>
2019-06-04 16:55:51 +0200

    dziala przycisk ruch

--- static/js/Ui.js
+++ static/js/Ui.js
@@ -48,5 +48,16 @@ class Ui {
                     break;
             }
         }
+        $("#move").on("click", function () {
+            if (game.rolling == false) {
+                var currentNumber = Math.floor(Math.random() * 6) + 1
+                game.currentNumber = currentNumber
+                console.log(currentNumber)
+                game.rolling = true
+                setTimeout(function () {
+                    game.rolling = false
+                }, 2000)
+            }
+        })
     }
 }
