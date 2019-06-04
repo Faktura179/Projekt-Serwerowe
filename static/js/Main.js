5f8e667d36e573d5077f267b3f6c924c92ac298c
mikord45 <51155742+mikord45@users.noreply.github.com>
2019-06-04 21:12:11 +0200

    jest focuus

--- static/js/Main.js
+++ static/js/Main.js
@@ -1,8 +1,9 @@
 var game
 var ui
 var net
-$(document).ready(function(){
+$(document).ready(function () {
     game = new Game()
     ui = new Ui()
     net = new Net()
-})
+    $("#move").focus()
+}) 
