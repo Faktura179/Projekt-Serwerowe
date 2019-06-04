d54369d5b6b92c5bc38d1b3c82a73a62037f9b64
mikord45 <51155742+mikord45@users.noreply.github.com>
2019-06-04 21:55:09 +0200

    nic

--- static/js/Net.js
+++ static/js/Net.js
@@ -1,34 +1,34 @@
-class Net{
-    constructor(){
+class Net {
+    constructor() {
         var socket
         socket = io()
-        this.io=socket
-        this.myMove=false
-        socket.on("conn",function(data){
+        this.io = socket
+        this.myMove = false
+        socket.on("conn", function (data) {
             console.log(data)
-            this.player=data.player-1
-            if(this.player==0){
-                this.myMove=true
-            }else{
+            this.player = data.player - 1
+            if (this.player == 0) {
+                this.myMove = true
+            } else {
                 alert("Czekaj na ruch przeciwnika")
             }
             console.log(this.myMove)
         }.bind(this))
-        socket.on("win",function(data){
-            socket.emit("win",{})
+        socket.on("win", function (data) {
+            socket.emit("win", {})
             alert("Wygrałeś!")
         })
-        socket.on("move",function(data){
-            this.myMove=true
-            if(this.player==0){
+        socket.on("move", function (data) {
+            this.myMove = true
+            if (this.player == 0) {
                 game.players[1].move(data.ilePol)
-            }else{
+            } else {
                 game.players[0].move(data.ilePol)
             }
         }.bind(this))
     }
 
-    move(argumenty){
-        this.io.emit("move",argumenty)
+    move(argumenty) {
+        this.io.emit("move", argumenty)
     }
 }
